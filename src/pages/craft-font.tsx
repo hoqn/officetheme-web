5f93607a55bb76d94427427f28cfdbea51f0a354
Hogyun Jeon <akzm9999@gmail.com>
2024-06-27 04:02:08 +0900

    feat: transition

--- src/pages/craft-font.tsx
+++ src/pages/craft-font.tsx
@@ -4,7 +4,7 @@ import { MainLayout } from "@/components/layout";
 export default function CraftFontSchemePage() {
   return (
     <MainLayout>
-      <main className="container pt-4">
+      <main className="container pt-4 animate-in fade-in-0 duration-300 ease-out slide-in-from-bottom-8">
         <FontSchemeGenSection className="mb-24" />
       </main>
     </MainLayout>
