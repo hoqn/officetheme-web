0ff3d1a73f60f0923bce4184e332ce49be5df66f
Hogyun Jeon <akzm9999@gmail.com>
2024-06-27 01:32:56 +0900

    fix: remove font styles from the selection

--- src/components/font-scheme/font-select.tsx
+++ src/components/font-scheme/font-select.tsx
@@ -4,26 +4,18 @@ import { useEffect, useState } from "react";
 import { useLocalFonts } from "./local-fonts-provider";
 
 export default function FontSelect({ onChange, value }: { onChange(value: string): void; value: string }) {
-  const { fontsByFamily, fontsRequestNotSupported, fonts } = useLocalFonts();
+  const { fontsByFamily, fontsRequestNotSupported } = useLocalFonts();
 
   const [options, setOptions] = useState<AutoCompleteOption[] | null>(null);
 
   useEffect(() => {
-    if (fonts.length)
-      setOptions(
-        Object.keys(fontsByFamily)
-          .map((it) => ({
-            value: it,
-            label: <span style={{ fontFamily: it }}>{it}</span>,
-          }))
-          .concat(
-            fonts.map((it) => ({
-              value: `${it.family} ${it.style}`,
-              label: <span style={{ fontFamily: it.postscriptName }}>{`${it.family} ${it.style}`}</span>,
-            }))
-          )
-      );
-  }, [fonts]);
+    setOptions(
+      Object.keys(fontsByFamily).map((it) => ({
+        value: it,
+        label: <span style={{ fontFamily: it }}>{it}</span>,
+      }))
+    );
+  }, [fontsByFamily]);
 
   const handleOnChange: React.ChangeEventHandler<HTMLInputElement> = (ev) => {
     onChange(ev.target.value);
